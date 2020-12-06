60b919f65d173aea9e94064b22251118549ccd87
Blue <urbi12misko@gmail.com>
2020-12-06 18:54:38 +0100

    Day 6 part 1 2020

---
+++ 2020/Day 6 part 1.py
@@ -0,0 +1,19 @@
+f = open("2020/Day 6.txt","r")
+
+currentGroup = ""
+total = 0
+for line in f:
+    line = line.rstrip()
+    if line == "":
+        total = total + len(set(currentGroup))
+        print(currentGroup + ": " + str(len(set(currentGroup))))
+        currentGroup = ""
+    else:
+        currentGroup = currentGroup + line
+
+if currentGroup != "":
+    total = total + len(set(currentGroup))
+    print(currentGroup + ": " + str(len(set(currentGroup))))
+    currentGroup = ""
+
+print(total)
